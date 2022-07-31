2485950f5866970b45568cee2543294ef03353f8
Matjah Sonneveld <matjah@matjah.eu>
2022-07-31 11:31:09 +0200

    chore: run linter

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,23 +1,19 @@
 // Simple Data-fetching
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from "react";
+import * as React from 'react'
 import {
   fetchPokemon,
   PokemonDataView,
   PokemonErrorBoundary,
-  PokemonInfoFallback
-} from "../pokemon";
-import {
-  createResource
-} from "../utils";
+  PokemonInfoFallback,
+} from '../pokemon'
+import {createResource} from '../utils'
 
-const pokemonResource = createResource(
-  fetchPokemon("pikachu")
-);
+const pokemonResource = createResource(fetchPokemon('pikachu'))
 
 function PokemonInfo() {
-  const pokemon = pokemonResource.read();
+  const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -25,11 +21,10 @@ function PokemonInfo() {
       </div>
       <PokemonDataView pokemon={pokemon} />
     </div>
-  );
+  )
 }
 
 function App() {
-
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
@@ -40,7 +35,7 @@ function App() {
         </PokemonErrorBoundary>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
